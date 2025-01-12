488ec5ed7052ab73b9afeabcf2158788c41f596b
ditmfv <ditmfvv@gmail.com>
2025-01-11 16:58:13 -0800

    Initial commit

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const categoriesList = document.querySelectorAll('#categories .item');
+console.log(`Number of categories: ${categoriesList.length}`);
+
+categoriesList.forEach(item => {
+  const categoryName = item.querySelector('h2').textContent;
+  const categoryItems = item.querySelectorAll('ul li').length;
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${categoryItems}`);
+});
