75ef973940a1d62b68a17f841c6ed6de9c24207c
ditmfv <ditmfvv@gmail.com>
2025-02-24 23:46:36 -0800

    new

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,11 @@
 const inputName = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
-// console.log(inputName);
+
+nameOutput.textContent = "Anonymous";
 
 inputName.addEventListener("input", handleInput);
 
 function handleInput(event) {
-  //   console.log(event.data);
-  //   console.log(event.currentTarget.value);
   const word = event.currentTarget.value.trim();
   if (!word) {
     nameOutput.textContent = "Anonymous";
@@ -14,7 +13,4 @@ function handleInput(event) {
   } else {
     nameOutput.textContent = word;
   }
-
-  //   const trimmedValue = inputName.value.trim();
-  //   nameOutput.textContent = trimmedValue || "Anonymous";
 }
--- js/task-6.js
+++ js/task-6.js
@@ -3,34 +3,35 @@ const buttonCreate = document.querySelector("button[data-create]");
 const buttonDestroy = document.querySelector("button[data-destroy]");
 const boxes = document.querySelector("#boxes");
 
-// input.addEventListener("input", handleInput);
 buttonCreate.addEventListener("click", createBoxes);
 buttonDestroy.addEventListener("click", destroyBoxes);
 
-function createBoxes(amount) {
-  // console.log(amount);
-  // console.log(input.value);
+function createBoxes() {
   const score = input.value;
   if (score < 1 || score > 100) {
     return;
   }
 
   let widthHeight = 30;
-  boxes.innerHTML = "";
+  const fragment = document.createDocumentFragment(); 
 
-  for (let i = 1; i <= score; i += 1) {
-    // `<div width="${widthHeight}" height="${widthHeight}"></div>`;
+  for (let i = 1; i <= score; i++) {
     const div = document.createElement("div");
     div.style.width = `${widthHeight}px`;
     div.style.height = `${widthHeight}px`;
     div.style.backgroundColor = getRandomHexColor();
     widthHeight += 10;
-    boxes.appendChild(div);
+    fragment.appendChild(div); 
   }
+
+  boxes.innerHTML = ""; 
+  boxes.appendChild(fragment); 
+
+  input.value = ""; 
 }
 
 function destroyBoxes() {
-  boxes.innerHTML = "";
+  boxes.innerHTML = ""; 
 }
 
 function getRandomHexColor() {
